763fd8dc08a850561a81e5a87128a941092d1341
veronicachen0912 <veroniachen0912@gmail.com>
2016-06-29 17:32:05 +0900

    upload

--- public/js/index.js
+++ public/js/index.js
@@ -3,19 +3,26 @@ function addText(){
 
    $('#uploadFile').trigger('click');
 
-   var file = document.getElementById('uploadFile').files[0];
-   var all   = $.when(file);   
-   all.done(function(){
-   	console.log("here");
-    if (file) {
-        // create reader
-        console.log("yes");
-        var reader = new FileReader();
-        reader.readAsText(file);
-        reader.onload = function(e) {
+   $('#uploadFile').on('change',function(){
+   
 
-         document.getElementById("content").innerHTML=e.target.result;
-        };
-    }
-	});
+       var file = document.getElementById('uploadFile').files[0];
+
+     var all   = $.when(file);   
+     all.done(function(){
+      
+        if (file) {
+          // create reader
+         
+          var reader = new FileReader();
+          reader.readAsText(file);
+          reader.onload = function(e) {
+            document.getElementById("content").innerHTML=e.target.result;
+          };
+        }
+    });
+
+  
+  });
+
 }
